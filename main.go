f58c1bf82e536877d29be4b1ebc700ce6a2b88d0
Sorawit Trutsat <sorawit.tr@gmail.com>
2024-10-13 18:41:35 +0700

    Handle GET book by ID

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,10 +17,24 @@ var books []Book
 func main() {
 	app := fiber.New()
 	app.Get("books", handleGetBooks)
+	app.Get("book/:ID", handleGetBook)
 	books = append(books, Book{ID: 1, Name: "kong", Author: "kong"})
 	app.Listen(":8080")
 }
 
-func handleGetBooks(fiber *fiber.Ctx) error {
-	return fiber.Status(200).JSON(books)
+func handleGetBooks(c *fiber.Ctx) error {
+	return c.Status(200).JSON(books)
+}
+
+func handleGetBook(c *fiber.Ctx) error {
+	bookID, error := strconv.Atoi(c.Params("ID"))
+	if error != nil {
+		return c.Status(400).SendString(fiber.ErrBadRequest.Error())
+	}
+	for _, book := range books {
+		if book.ID == bookID {
+			return c.JSON(book)
+		}
+	}
+	return c.Status(fiber.StatusBadRequest).SendString("Book not found")
 }
